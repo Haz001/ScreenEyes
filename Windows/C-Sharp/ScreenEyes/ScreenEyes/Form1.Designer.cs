475b0aaa2da9b0957cb7ec922e1487e5a3003b5a
Haz <24993938+Haz001@users.noreply.github.com>
2019-01-06 20:07:10 +0000

    Making
    Made icons and making the basic application

--- Windows/C-Sharp/ScreenEyes/ScreenEyes/Form1.Designer.cs
+++ Windows/C-Sharp/ScreenEyes/ScreenEyes/Form1.Designer.cs
@@ -29,12 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.working_btn = new System.Windows.Forms.Button();
+            this.break_btn = new System.Windows.Forms.Button();
+            this.settings_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.second = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // working_btn
+            // 
+            this.working_btn.Location = new System.Drawing.Point(12, 12);
+            this.working_btn.Name = "working_btn";
+            this.working_btn.Size = new System.Drawing.Size(75, 23);
+            this.working_btn.TabIndex = 0;
+            this.working_btn.Text = "Working";
+            this.working_btn.UseVisualStyleBackColor = true;
+            // 
+            // break_btn
+            // 
+            this.break_btn.Location = new System.Drawing.Point(13, 41);
+            this.break_btn.Name = "break_btn";
+            this.break_btn.Size = new System.Drawing.Size(75, 23);
+            this.break_btn.TabIndex = 0;
+            this.break_btn.Text = "break_btn";
+            this.break_btn.UseVisualStyleBackColor = true;
+            // 
+            // settings_btn
+            // 
+            this.settings_btn.Location = new System.Drawing.Point(12, 70);
+            this.settings_btn.Name = "settings_btn";
+            this.settings_btn.Size = new System.Drawing.Size(75, 23);
+            this.settings_btn.TabIndex = 0;
+            this.settings_btn.Text = "Settings";
+            this.settings_btn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // user_notify
+            // 
+            this.user_notify.Icon = ((System.Drawing.Icon)(resources.GetObject("user_notify.Icon")));
+            this.user_notify.Text = "notifyIcon1";
+            this.user_notify.Visible = true;
+            // 
+            // second
+            // 
+            this.second.Enabled = true;
+            this.second.Interval = 1000;
+            this.second.Tick += new System.EventHandler(this.second_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(100, 120);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.settings_btn);
+            this.Controls.Add(this.break_btn);
+            this.Controls.Add(this.working_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button working_btn;
+        private System.Windows.Forms.Button break_btn;
+        private System.Windows.Forms.Button settings_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon user_notify;
+        private System.Windows.Forms.Timer second;
     }
 }
 
--- Windows/C-Sharp/ScreenEyes/ScreenEyes/Form1.cs
+++ Windows/C-Sharp/ScreenEyes/ScreenEyes/Form1.cs
@@ -12,9 +12,33 @@ namespace ScreenEyes
 {
     public partial class Form1 : Form
     {
+        int count_st = 0;
+        int soft_st = 50*60;
+        int hard_st = 60 * 60;
+        int break_t = 10*60;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void second_Tick(object sender, EventArgs e)
+        {
+            count_st += 1;
+            if(soft_st <= count_st)
+            {
+                user_notify.ShowBalloonTip(1000, "Think about taking a break","It is coming up to the time to take a break to reduce eye strain. Save and start your "+(break_t/60f).ToString()+" minute break. A automatic break will start in "+((hard_st-soft_st)/60f).ToString()+" minutes if you don't start your break yourself.",ToolTipIcon.Info);
+
+            }
+            else
+            {
+                user_notify.ShowBalloonTip(1000, "Think about taking a break", "It is coming up to the time to take a break to reduce eye strain. Save and start your " + (break_t / 60f).ToString() + " minute break. A automatic break will start in " + ((hard_st - soft_st) / 60f).ToString() + " minutes if you don't start your break yourself.", ToolTipIcon.Info);
+            }
         }
     }
 }
